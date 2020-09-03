e15fbf6848286bb6a9c54ef27af1c0debb98df9b
Mohamed Bakoush <bakoushmohamed@gmail.com>
2020-09-03 12:06:23 +0100

    comments
    comments to explain what each header dose

--- server.js
+++ server.js
@@ -8,48 +8,55 @@ const app = express();
 // show website icon on tab
 app.use(favicon(path.join(__dirname, "client", "images", "favicon", "favicon.ico")));
 
+// get video thumbnail by video id and thumbnail number header
 app.get("/thumbnail/:videoID/:thumbnailID", streamImageById);
 function streamImageById(req, res){
   streamVideoFile.streamThumbnail(req, res, req.params.videoID, req.params.thumbnailID);
 }
 
+// delete video permently by video id header
 app.get("/delete-video-data-permanently/:id", deletevideoData);
 function deletevideoData(req, res){
   streamVideoFile.deletevideoData(req, res, req.params.id);
 }
 
+// stream video by video id header
 app.get("/video/:id", streamVideoById);
 function streamVideoById(req, res){
   streamVideoFile.streamVideo(req, res, req.params.id);
 }
 
+// get video data for specified video by id header
 app.get("/video-data/:id", findVideosByID);
 function findVideosByID(req, res){
   res.json(streamVideoFile.findVideosByID(req.params.id));
 }
 
-
+// get all available video data header
 app.get("/all-available-video-data", getAllAvailableVideos);
 function getAllAvailableVideos(req, res){
   res.json(streamVideoFile.getAllAvailableVideos());
 }
 
-
+// download live video stream header
 app.post("/downloadVideoStream", express.json(), downloadVideoStream);
 function downloadVideoStream(req, res){
   streamVideoFile.downloadVideoStream(req, res);
 }
 
+// download video header
 app.post("/downloadVideo", express.json(), downloadVideo);
 function downloadVideo(req, res){
   streamVideoFile.downloadVideo(req, res);
 }
 
+// download video from specified section header
 app.post("/trimVideo", express.json(), trimVideo);
 function trimVideo(req, res){
   streamVideoFile.trimVideo(req, res);
 }
 
+// stop downloading live video stream header
 app.post("/stopDownloadVideoStream", express.json(), stopDownloadVideoStream);
 function stopDownloadVideoStream(req, res){
   streamVideoFile.stopDownloadVideoStream(req, res);
