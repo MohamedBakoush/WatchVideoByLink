c55a236a3d3771bfd46f2ff7bb03d6a8961b0f68
Mohamed Bakoush <bakoushmohamed@gmail.com>
2022-01-22 11:57:41 +0100

    function: update_json_path_validity

---
+++ backend/scripts/check-path-validity.js
@@ -0,0 +1,25 @@
+"use strict"; 
+const path = require("path");
+const FileSystem = require("fs"); 
+
+// check validity of json path
+function update_json_path_validity(newPath) {
+    if (FileSystem.existsSync(newPath)) {
+        try {
+        if (path.extname(newPath) === ".json") { 
+            return "valid path";
+        } else {
+            return "input path not json"; 
+        }
+        } catch (error) {
+        return error;
+        }
+    } else {
+        return "invalid path";
+    }
+}
+
+
+module.exports = { // export modules 
+    update_json_path_validity
+};
