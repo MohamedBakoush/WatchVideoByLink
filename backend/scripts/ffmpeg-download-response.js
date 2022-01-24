2b87daa029b8a1cf7489ff15970e841478ed2478
Mohamed Bakoush <bakoushmohamed@gmail.com>
2022-01-24 23:01:00 +0100

    getDownloadResponse

--- backend/scripts/ffmpeg-download-response.js
+++ backend/scripts/ffmpeg-download-response.js
@@ -1,1 +1,33 @@
 "use strict";
+let downloadResponse = {}; 
+
+function getDownloadResponse(path_array){
+    if (Array.isArray(path_array)) {
+        if (path_array.length !== 0) {
+        let dataPath = "downloadResponse";
+        for (let i = 0; i < path_array.length; i++) { 
+            if (i == path_array.length - 1) { 
+                try {
+                if (eval(dataPath)[path_array[i]] !== undefined) {
+                    return eval(dataPath)[path_array[i]];
+                } else {
+                    return undefined;
+                }
+                } catch (error) {
+                return undefined;
+                }
+            } else  { 
+                dataPath += `[path_array[${i}]]`;
+            }
+        } 
+        } else {
+        return undefined;
+        }
+    } else  { 
+        return downloadResponse;
+    } 
+}
+
+module.exports = { // export modules   
+    getDownloadResponse
+};
