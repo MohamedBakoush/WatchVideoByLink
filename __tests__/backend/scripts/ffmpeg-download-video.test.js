a0d637e6cf44eb43a4a04391031f846bd8bc799f
Mohamed Bakoush <bakoushmohamed@gmail.com>
2022-01-25 23:44:52 +0100

    test checkIfVideoSrcOriginalPathExits: ffmpeg-download-video -> data-videos

--- __tests__/backend/scripts/ffmpeg-download-video.test.js
+++ __tests__/backend/scripts/ffmpeg-download-video.test.js
@@ -5,25 +5,6 @@ const currentDownloadVideos = require("../../../backend/scripts/current-download
 const currentDownloadVideos_json_path = "__tests__/data/current-download-videos.test.json";
 const { v4: uuidv4 } = require("uuid");
 
-const dataVideos_data = {
-    "video": {
-        "originalVideoSrc" : "videoSrc",
-        "originalVideoType" : "videoType",
-        "path": "videoFilePath",
-        "videoType" : "video/mp4",
-        "download" : "completed",
-      },
-      "compression" : {
-        "path": "compressionFilePath",
-        "videoType": "video/webm",
-        "download": "completed"
-      },
-      "thumbnail": {
-        "path": {},
-        "download": "completed"
-      }
-};
-
 beforeAll(() => {    
     dataVideos.update_data_videos_path(dataVideos_json_path); 
     dataVideos.resetVideoData();
@@ -34,61 +15,6 @@ beforeAll(() => {
 afterEach(() => {    
     dataVideos.resetVideoData();
     currentDownloadVideos.resetCurrentDownloadVideos();
-}); 
-
-describe("checkIfVideoSrcOriginalPathExits", () =>  {   
-    it("No Input", async () =>  {
-        const checkIfVideoSrcOriginalPathExits = await ffmpegDownloadVideo.checkIfVideoSrcOriginalPathExits();
-        expect(checkIfVideoSrcOriginalPathExits).toBe(undefined);
-    });  
-        
-    it("Input String", async () =>  {
-        const checkIfVideoSrcOriginalPathExits = await ffmpegDownloadVideo.checkIfVideoSrcOriginalPathExits("test");
-        expect(checkIfVideoSrcOriginalPathExits).toBe("test");
-    });  
-
-    it("Input URL", async () =>  {
-        const checkIfVideoSrcOriginalPathExits = await ffmpegDownloadVideo.checkIfVideoSrcOriginalPathExits("http://localhost:8080/test.mp4");
-        expect(checkIfVideoSrcOriginalPathExits).toBe("http://localhost:8080/test.mp4");
-    });  
-
-    it("/video/: Invalid fileName", async () =>  { 
-        const checkIfVideoSrcOriginalPathExits = await ffmpegDownloadVideo.checkIfVideoSrcOriginalPathExits("http://localhost:8080/video/invalid");
-        expect(checkIfVideoSrcOriginalPathExits).toBe("http://localhost:8080/video/invalid");
-    }); 
-
-    it("/compressed/: Invalid fileName", async () =>  { 
-        const checkIfVideoSrcOriginalPathExits = await ffmpegDownloadVideo.checkIfVideoSrcOriginalPathExits("http://localhost:8080/compressed/invalid");
-        expect(checkIfVideoSrcOriginalPathExits).toBe("http://localhost:8080/compressed/invalid");
-    }); 
-
-    it("/video/: Valid fileName, Invalid Data", async () =>  {
-        const fileName = uuidv4();
-        dataVideos.updateVideoData([fileName], undefined);
-        const checkIfVideoSrcOriginalPathExits = await ffmpegDownloadVideo.checkIfVideoSrcOriginalPathExits(`http://localhost:8080/video/${fileName}`);
-        expect(checkIfVideoSrcOriginalPathExits).toBe(`http://localhost:8080/video/${fileName}`);
-    }); 
-
-    it("/compressed/: Valid fileName, Invalid Data", async () =>  {
-        const fileName = uuidv4();
-        dataVideos.updateVideoData([fileName], undefined);
-        const checkIfVideoSrcOriginalPathExits = await ffmpegDownloadVideo.checkIfVideoSrcOriginalPathExits(`http://localhost:8080/compressed/${fileName}`);
-        expect(checkIfVideoSrcOriginalPathExits).toBe(`http://localhost:8080/compressed/${fileName}`);
-    }); 
-
-    it("/video/: Valid fileName, Valid Data", async () =>  {
-        const fileName = uuidv4();
-        dataVideos.updateVideoData([fileName], dataVideos_data);
-        const checkIfVideoSrcOriginalPathExits = await ffmpegDownloadVideo.checkIfVideoSrcOriginalPathExits(`http://localhost:8080/video/${fileName}`);
-        expect(checkIfVideoSrcOriginalPathExits).toBe("videoFilePath");
-    }); 
-
-    it("/compressed/: Valid fileName, Valid Data", async () =>  {
-        const fileName = uuidv4();
-        dataVideos.updateVideoData([fileName], dataVideos_data);
-        const checkIfVideoSrcOriginalPathExits = await ffmpegDownloadVideo.checkIfVideoSrcOriginalPathExits(`http://localhost:8080/compressed/${fileName}`);
-        expect(checkIfVideoSrcOriginalPathExits).toBe("videoFilePath");
-    }); 
 }); 
 
 describe("start_downloadVideo", () =>  {   
