f1147e8dc4810870345c6f9ec507340ade4daa4e
Mohamed Bakoush <bakoushmohamed@gmail.com>
2022-01-28 19:38:16 +0100

    set-up: ffmpeg-download-stream.test.js

---
+++ __tests__/backend/scripts/ffmpeg-download-stream.test.js
@@ -0,0 +1,18 @@
+const ffmpegDownloadStream = require("../../../backend/scripts/ffmpeg-download-stream");
+const dataVideos = require("../../../backend/scripts/data-videos");
+const dataVideos_json_path = "__tests__/data/data-videos.test.json";
+const currentDownloadVideos = require("../../../backend/scripts/current-download-videos");
+const currentDownloadVideos_json_path = "__tests__/data/current-download-videos.test.json";
+const { v4: uuidv4 } = require("uuid");
+
+beforeAll(() => {    
+    dataVideos.update_data_videos_path(dataVideos_json_path); 
+    dataVideos.resetVideoData();
+    currentDownloadVideos.update_current_download_videos_path(currentDownloadVideos_json_path); 
+    currentDownloadVideos.resetCurrentDownloadVideos();
+});
+
+afterEach(() => {    
+    dataVideos.resetVideoData();
+    currentDownloadVideos.resetCurrentDownloadVideos();
+}); 
