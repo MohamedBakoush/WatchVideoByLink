b3a1d927ecd47a12eb0b20dc4ece1c6d8437fd79
Mohamed Bakoush <bakoushmohamed@gmail.com>
2022-01-29 21:31:09 +0100

    update_download_compression_fileNameID: update variable name

--- __tests__/backend/scripts/ffmpeg-download-compression.test.js
+++ __tests__/backend/scripts/ffmpeg-download-compression.test.js
@@ -39,34 +39,34 @@ describe("get_download_compression_fileNameID", () =>  {
 
 describe("update_download_compression_fileNameID", () =>  {    
     it("No Input", () =>  {
-        const updateStreamID = ffmpegDownloadCompression.update_download_compression_fileNameID();
-        expect(updateStreamID).toBe("fileNameID not string");
+        const updateCompressionID = ffmpegDownloadCompression.update_download_compression_fileNameID();
+        expect(updateCompressionID).toBe("fileNameID not string");
     });     
 
     it("Number Input", () =>  {
-        const updateStreamID = ffmpegDownloadCompression.update_download_compression_fileNameID(12);
-        expect(updateStreamID).toBe("fileNameID not string");
+        const updateCompressionID = ffmpegDownloadCompression.update_download_compression_fileNameID(12);
+        expect(updateCompressionID).toBe("fileNameID not string");
     });     
 
     it("Array Input", () =>  {
-        const updateStreamID = ffmpegDownloadCompression.update_download_compression_fileNameID([]);
-        expect(updateStreamID).toBe("fileNameID not string");
+        const updateCompressionID = ffmpegDownloadCompression.update_download_compression_fileNameID([]);
+        expect(updateCompressionID).toBe("fileNameID not string");
     });     
 
     it("Object Input", () =>  {
-        const updateStreamID = ffmpegDownloadCompression.update_download_compression_fileNameID({});
-        expect(updateStreamID).toBe("fileNameID not string");
+        const updateCompressionID = ffmpegDownloadCompression.update_download_compression_fileNameID({});
+        expect(updateCompressionID).toBe("fileNameID not string");
     }); 
 
     it("Boolean Input", () =>  {
-        const updateStreamID = ffmpegDownloadCompression.update_download_compression_fileNameID(true);
-        expect(updateStreamID).toBe("fileNameID not string");
+        const updateCompressionID = ffmpegDownloadCompression.update_download_compression_fileNameID(true);
+        expect(updateCompressionID).toBe("fileNameID not string");
     });     
 
     it("String Input", () =>  {
         const fileName = uuidv4();
-        const updateStreamID = ffmpegDownloadCompression.update_download_compression_fileNameID(fileName);
-        expect(updateStreamID).toBe(fileName);
+        const updateCompressionID = ffmpegDownloadCompression.update_download_compression_fileNameID(fileName);
+        expect(updateCompressionID).toBe(fileName);
     });     
 });
 
