20a899f744e1afe46fa6b6fe9232f246956f73db
Mohamed Bakoush <bakoushmohamed@gmail.com>
2022-01-29 17:09:09 +0100

    set-up: ffmpeg-download-image.test.js

---
+++ __tests__/backend/scripts/ffmpeg-download-image.test.js
@@ -0,0 +1,22 @@
+const ffmpegDownloadImage = require("../../../backend/scripts/ffmpeg-download-image");
+const dataVideos = require("../../../backend/scripts/data-videos");
+const dataVideos_json_path = "__tests__/data/data-videos.test.json";
+const availableVideos = require("../../../backend/scripts/available-videos");
+const availableVideos_json_path = "__tests__/data/available-videos.test.json";
+const currentDownloadVideos = require("../../../backend/scripts/current-download-videos");
+const currentDownloadVideos_json_path = "__tests__/data/current-download-videos.test.json";
+
+beforeAll(() => {    
+    dataVideos.update_data_videos_path(dataVideos_json_path); 
+    dataVideos.resetVideoData();
+    availableVideos.update_available_videos_path(availableVideos_json_path); 
+    availableVideos.resetAvailableVideos();
+    currentDownloadVideos.update_current_download_videos_path(currentDownloadVideos_json_path); 
+    currentDownloadVideos.resetCurrentDownloadVideos();
+});
+
+afterEach(() => {    
+    dataVideos.resetVideoData();
+    availableVideos.resetAvailableVideos();
+    currentDownloadVideos.resetCurrentDownloadVideos();
+}); 
