060d88d35f54dc3cc5ae1fec81a821171b0998d0
Mohamed Bakoush <bakoushmohamed@gmail.com>
2022-01-30 02:27:39 +0100

    test: getVideoLinkFromUrl

--- __tests__/backend/scripts/youtubedl-download-video.test.js
+++ __tests__/backend/scripts/youtubedl-download-video.test.js
@@ -1,1 +1,44 @@
 const youtubedl = require("../../../backend/scripts/youtubedl-download-video");
+const ffmpegDownloadResponse = require("../../../backend/scripts/ffmpeg-download-response");
+
+beforeAll(() => {    
+    ffmpegDownloadResponse.resetDownloadResponse();
+});
+
+afterEach(() => {    
+    ffmpegDownloadResponse.resetDownloadResponse();
+}); 
+
+describe("getVideoLinkFromUrl", () =>  {  
+    it("No Input", async () =>  {
+        const getVideoLinkFromUrl = await youtubedl.getVideoLinkFromUrl();
+        expect(getVideoLinkFromUrl).toBe("failed-get-video-url-from-provided-url");  
+    });
+
+    it("URL: Number", async () =>  {
+        const getVideoLinkFromUrl = await youtubedl.getVideoLinkFromUrl(123);
+        expect(getVideoLinkFromUrl).toBe("failed-get-video-url-from-provided-url"); 
+    });
+
+    it("URL: Array", async () =>  {
+        const getVideoLinkFromUrl = await youtubedl.getVideoLinkFromUrl([]);
+        expect(getVideoLinkFromUrl).toBe("failed-get-video-url-from-provided-url"); 
+    });
+
+    it("URL: Object", async () =>  {
+        const getVideoLinkFromUrl = await youtubedl.getVideoLinkFromUrl({});
+        expect(getVideoLinkFromUrl).toBe("failed-get-video-url-from-provided-url"); 
+    });
+
+    it("URL: Bool", async () =>  {
+        const getVideoLinkFromUrl = await youtubedl.getVideoLinkFromUrl(true);
+        expect(getVideoLinkFromUrl).toBe("failed-get-video-url-from-provided-url"); 
+    });
+
+    it("URL: Invalid URL", async () =>  {
+        const getVideoLinkFromUrl = await youtubedl.getVideoLinkFromUrl("URL");
+        expect(getVideoLinkFromUrl.message).toBe("initializing");  
+        const getDownloadResponse = ffmpegDownloadResponse.getDownloadResponse([getVideoLinkFromUrl.fileName]);
+        expect(getDownloadResponse.message).toBe("initializing"); 
+    });
+}); 
--- backend/scripts/youtubedl-download-video.js
+++ backend/scripts/youtubedl-download-video.js
@@ -5,64 +5,77 @@ const ffmpegDownloadResponse = require("./ffmpeg-download-response");
 
 // using youtube-dl it converts url link to video type and video src
 async function getVideoLinkFromUrl(url) {
-  const fileName = uuidv4();
-  try {
+  if (typeof url !== "string") {
+    return "failed-get-video-url-from-provided-url";
+  } else {
+    const fileName = uuidv4();
     ffmpegDownloadResponse.updateDownloadResponse([fileName], {
       "fileName": fileName,
       "message": "initializing"
     });
-    // Optional arguments passed to youtube-dl.
-    const options = ["--skip-download"];
-    youtubedl.getInfo(url, options, function(err, info) {
-      // info.protocol
-      // protocol: https or http == video/mp4
-      // protocol: http_dash_segments == application/dash+xml
-      // protocol: m3u8 == application/x-mpegURL
-      let videoFileFormat, videoUrlLink;
-      if (info !== undefined) {
-        if (info.protocol == "https" || info.protocol == "http") {
-          videoUrlLink = info.url;
-          videoFileFormat = "video/mp4";
-        } else if (info.protocol == "m3u8") {
-          videoUrlLink = info.url;
-          videoFileFormat = "application/x-mpegURL";
-        } else if (info.protocol == "http_dash_segments") {
-          videoUrlLink = info.url;
-          videoFileFormat = "application/dash+xml";
+    try {
+      // Optional arguments passed to youtube-dl.
+      const options = ["--skip-download"];
+      youtubedl.getInfo(url, options, function(err, info) {
+        // info.protocol
+        // protocol: https or http == video/mp4
+        // protocol: http_dash_segments == application/dash+xml
+        // protocol: m3u8 == application/x-mpegURL
+        let videoFileFormat, videoUrlLink;
+        if (info !== undefined) {
+          if (info.protocol == "https" || info.protocol == "http") {
+            videoUrlLink = info.url;
+            videoFileFormat = "video/mp4";
+          } else if (info.protocol == "m3u8") {
+            videoUrlLink = info.url;
+            videoFileFormat = "application/x-mpegURL";
+          } else if (info.protocol == "http_dash_segments") {
+            videoUrlLink = info.url;
+            videoFileFormat = "application/dash+xml";
+          } else {
+            videoUrlLink = "not-supported";
+            videoFileFormat = "not-supported";
+          }
         } else {
           videoUrlLink = "not-supported";
           videoFileFormat = "not-supported";
         }
-      } else {
-        videoUrlLink = "not-supported";
-        videoFileFormat = "not-supported";
-      }
-      const videoDataFromUrl = {
-        input_url_link: url,
-        video_url: videoUrlLink,
-        video_file_format: videoFileFormat
+        const videoDataFromUrl = {
+          input_url_link: url,
+          video_url: videoUrlLink,
+          video_file_format: videoFileFormat
+        };
+        if (videoUrlLink !== "not-supported" || videoFileFormat !== "not-supported") {
+          if (ffmpegDownloadResponse.getDownloadResponse([fileName, "message"]) !== undefined) {
+              ffmpegDownloadResponse.updateDownloadResponse([fileName, "message"], videoDataFromUrl);
+          }
+          return {
+              "fileName": fileName,
+              "message": videoDataFromUrl
+          };
+        } else {
+          if (ffmpegDownloadResponse.getDownloadResponse([fileName, "message"]) !== undefined) {
+              ffmpegDownloadResponse.updateDownloadResponse([fileName, "message"], "failed-get-video-url-from-provided-url");
+          }
+          return {
+              "fileName": fileName,
+              "message": "failed-get-video-url-from-provided-url"
+          };
+        }
+      });
+      return {
+          "fileName": fileName,
+          "message": "initializing"
       };
-      if (videoUrlLink !== "not-supported" || videoFileFormat !== "not-supported") {
-        if (ffmpegDownloadResponse.getDownloadResponse([fileName, "message"]) !== undefined) {
-            ffmpegDownloadResponse.updateDownloadResponse([fileName, "message"], videoDataFromUrl);
-        }
-      } else {
-        if (ffmpegDownloadResponse.getDownloadResponse([fileName, "message"]) !== undefined) {
-            ffmpegDownloadResponse.updateDownloadResponse([fileName, "message"], "failed-get-video-url-from-provided-url");
-        }
-      }
-    });
-    return {
+    } catch (e) {
+      if (ffmpegDownloadResponse.getDownloadResponse([fileName, "message"]) !== undefined) {
+          ffmpegDownloadResponse.updateDownloadResponse([fileName, "message"], "failed-get-video-url-from-provided-url");
+      } 
+      return {
         "fileName": fileName,
-        "message": "initializing"
-    };
-  } catch (e) {
-    if (ffmpegDownloadResponse.getDownloadResponse([fileName, "message"]) !== undefined) {
-        ffmpegDownloadResponse.updateDownloadResponse([fileName, "message"], "failed-get-video-url-from-provided-url");
-    } 
-    return {
-      "message": "failed-get-video-url-from-provided-url"
-    };
+        "message": "failed-get-video-url-from-provided-url"
+      };
+    }
   }
 }
 
