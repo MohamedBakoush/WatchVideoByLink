f205ec46f2b175459dac083a0a38ccccda5e8e72
Mohamed Bakoush <bakoushmohamed@gmail.com>
2022-02-18 14:54:43 +0100

    update untrunc: variables + rename_file

--- backend/scripts/ffmpeg-unfinished-videos.js
+++ backend/scripts/ffmpeg-unfinished-videos.js
@@ -224,22 +224,19 @@ function completeUnfinnishedVideoDownload(fileName){
 }
 
 // Restore a damaged (truncated) mp4 provided a similar not broken video is available
-function untrunc(fileName, fileType, newFilePath, path, fileName_original_ending, fileName_fixed_ending){
+function untrunc(fileName, fileType, fileName_path, video_path, fileName_original_ending, fileName_fixed_ending){
   const working_video_path = ffmpegPath.get_working_video_path();
-  const broken_video_path = `./media/video/${fileName}/${fileName}.mp4`;
-  if(deleteData.check_if_file_exits(fileName_original_ending) == true){  
+  const broken_video_path = `${fileName_path}/${fileName}.mp4`;
+  if(deleteData.check_if_file_exits(`${fileName_path}/${fileName_original_ending}`) == true){  
     untrunc_exec(working_video_path, broken_video_path, () => {
-      downloadVideoAfterUntrunc(fileName,fileType,newFilePath,path, fileName_original_ending, fileName_fixed_ending);
+      downloadVideoAfterUntrunc(fileName, fileType, fileName_path, video_path, fileName_original_ending, fileName_fixed_ending);
     });
-  } else if(deleteData.check_if_file_exits(fileName_fixed_ending) == true){ 
-    const renameFilePath = setInterval(function(){ 
-      FileSystem.rename(fileName_fixed_ending, fileName_original_ending,  () => { 
-        clearInterval(renameFilePath);
-        untrunc_exec(working_video_path, broken_video_path, () => {
-          downloadVideoAfterUntrunc(fileName,fileType,newFilePath,path, fileName_original_ending, fileName_fixed_ending);
-        });
+  } else if(deleteData.check_if_file_exits(`${fileName_path}/${fileName_fixed_ending}`) == true){ 
+    deleteData.rename_file(`${fileName_path}/${fileName_fixed_ending}`, fileName_path, fileName_original_ending,  () => { 
+      untrunc_exec(working_video_path, broken_video_path, () => {
+        downloadVideoAfterUntrunc(fileName, fileType, fileName_path, video_path, fileName_original_ending, fileName_fixed_ending);
       });
-    }, 50);
+    });
   } else{ 
     deleteData.deleteAllVideoData(fileName);       
   }
